49f4f7fc84574d1a04150328aa70abf0da2a3eb3
m <mn@klkl.de>
2017-05-31 13:55:26 +0200

    test commit

--- app/src/main/java/com/example/kevin/thebest/MainActivity.java
+++ app/src/main/java/com/example/kevin/thebest/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("I am alive");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
